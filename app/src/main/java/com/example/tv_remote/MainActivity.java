f804613571c71af328a2f2d171fe292e10e16355
Paul-Abel <59267403+Paul-Abel@users.noreply.github.com>
2020-03-04 15:49:31 +0100

    TestButton

--- app/src/main/java/com/example/tv_remote/MainActivity.java
+++ app/src/main/java/com/example/tv_remote/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.tv_remote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button testButton;
+    private TextView testText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testButton = (Button) findViewById(R.id.TestButton);
+        testText = (TextView) findViewById(R.id.TestText);
+
+        testButton.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View view){
+                    testText.setText("Es hat funktioniert");
+                }
+        });
     }
 }
