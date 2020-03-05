9d9761b2317355c28f33928226b179b94be1fed6
Paul-Abel <59267403+Paul-Abel@users.noreply.github.com>
2020-03-06 00:18:30 +0100

    fließender Farbwechsel für den Hintergrund
    fließender Farbwechsel für den Hintergrund

--- app/src/main/java/com/example/tv_remote/MainActivity.java
+++ app/src/main/java/com/example/tv_remote/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.tv_remote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        testButton = (Button) findViewById(R.id.Main_LedButton);
-        testText = (TextView) findViewById(R.id.MainHeader);
+        testButton = findViewById(R.id.Main_LedButton);
+        testText = findViewById(R.id.MainHeader);
 
         testButton.setOnClickListener(new View.OnClickListener(){
                 public void onClick(View v){
@@ -28,7 +30,11 @@ public class MainActivity extends AppCompatActivity {
                 }
         });
 
-
+        ConstraintLayout constraintLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
     }
     public void openled(){
         Intent led = new Intent(this, Led.class);
