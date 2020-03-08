72494e6c23907f459bf116819036d724292c2dfb
Paul-Abel <59267403+Paul-Abel@users.noreply.github.com>
2020-03-08 21:32:19 +0100

    Android update, Code sauber gemacht, funktionierende Toolbar implementiert

--- app/src/main/java/com/example/tv_remote/Led.java
+++ app/src/main/java/com/example/tv_remote/Led.java
@@ -1,18 +1,21 @@
 package com.example.tv_remote;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
 @RequiresApi(api = Build.VERSION_CODES.M)
 public class Led extends AppCompatActivity {
 
@@ -20,6 +23,7 @@ public class Led extends AppCompatActivity {
     private Button led_anzeige;
     private Button led_red;
     private Button led_blue;
+
 
 
 
@@ -31,19 +35,9 @@ public class Led extends AppCompatActivity {
         led_red = findViewById(R.id.led_red);
         led_blue = findViewById(R.id.led_blue);
         led_anzeige = findViewById(R.id.led_anzeige);
-        main = findViewById(R.id.LED_ChanceSite);
 
-        ConstraintLayout constraintLayout = findViewById(R.id.layout);
-        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
-        animationDrawable.setEnterFadeDuration(2000);
-        animationDrawable.setExitFadeDuration(4000);
-        animationDrawable.start();
-
-        main.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                mainmenu();
-            }
-        });
+        animatedbackground();
+        toolbar();
 
         led_red.setOnClickListener(new View.OnClickListener()
         {
@@ -51,23 +45,50 @@ public class Led extends AppCompatActivity {
                 aktualiesereFarbe(R.color.red);
             }
         });
-
         led_blue.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v){
                 aktualiesereFarbe(R.color.blue);
             }
         });
-
     }
 
-    public void mainmenu(){
-        Intent main = new Intent(this, MainActivity.class);
-        startActivity(main);
+    @Override   //Toolbarsytle wird eingefügt
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_example, menu);
+        return true;
     }
 
+    @Override   //Navigation
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.toolbar_television:
+                Intent television = new Intent(this, MainActivity.class);
+                startActivity(television);
+                return true;
+            case R.id.toolbar_reciver:
+                Intent reciver = new Intent(this, Reciver.class);
+                startActivity(reciver);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-    //requires API-Level 23 also mindestens Android 6 Marshmellow
+    public void toolbar(){  //Navigation wird eingefügt
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void animatedbackground(){
+        ConstraintLayout constraintLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+    }
+
     public void aktualiesereFarbe(int farbID){
         int Farbe = getColor(farbID);
         led_anzeige.setBackgroundColor(Farbe);
--- app/src/main/java/com/example/tv_remote/Reciver.java
+++ app/src/main/java/com/example/tv_remote/Reciver.java
@@ -3,29 +3,21 @@ package com.example.tv_remote;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button testButton;
-    private TextView testText;
+public class Reciver extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_reciver);
 
         toolbar();
-        animatedbackground();
     }
 
     @Override   //Toolbarsytle wird eingefügt
@@ -42,9 +34,9 @@ public class MainActivity extends AppCompatActivity {
                 Intent led = new Intent(this, Led.class);
                 startActivity(led);
                 return true;
-            case R.id.toolbar_reciver:
-                Intent reciver = new Intent(this, Reciver.class);
-                startActivity(reciver);
+            case R.id.toolbar_television:
+                Intent television = new Intent(this, MainActivity.class);
+                startActivity(television);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -55,13 +47,4 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
-
-    public void animatedbackground(){
-        ConstraintLayout constraintLayout = findViewById(R.id.layout);
-        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
-        animationDrawable.setEnterFadeDuration(2000);
-        animationDrawable.setExitFadeDuration(4000);
-        animationDrawable.start();
-    }
-
 }
