c866656e22dbf07508842087f10835fcee54aeb4
Elrexo <snake.randaltus@gmail.com>
2020-03-11 13:36:17 +0100

    LED ansteuern funktioniert
    Led an und Aus

--- app/src/main/java/com/example/tv_remote/InternetConnection.java
+++ app/src/main/java/com/example/tv_remote/InternetConnection.java
@@ -5,32 +5,27 @@ import android.os.AsyncTask;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.TimeUnit;
 
-public class InternetConnection extends AsyncTask<Void, Void, Void> {
+public class InternetConnection extends AsyncTask<String, Void, Void> {
+    private Socket s = null;
+    private DataOutputStream message = null;
+
 
     @Override
-    protected Void doInBackground(Void... voids) {
-        Socket s = null;
-        DataOutputStream message = null;
-
+    protected Void doInBackground(String... strings) {
+        String frequenz = strings[0];
         try {
             s = new Socket("192.168.2.122", 80);
             message = new DataOutputStream(s.getOutputStream());
-            MainActivity.
-            message.writeUTF("16769565X");
+            message.writeUTF(frequenz);
+            message.close();
+            s.close();
+
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-        try{
-            assert s != null;
-            s.close();
-            assert message != null;
-            message.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         return null;
     }
 }
