77820abee675c2c44b93580cad5f71742cb7a0a1
Elrexo <snake.randaltus@gmail.com>
2020-03-12 12:21:13 +0100

    LED ansteuern funktioniert

--- app/src/main/java/com/example/tv_remote/InternetConnection.java
+++ app/src/main/java/com/example/tv_remote/InternetConnection.java
@@ -26,4 +26,19 @@ public class InternetConnection extends AsyncTask<String, Void, Void> {
         }
         return null;
     }
+    public void test(String frequenz){
+         Socket t = null;
+         DataOutputStream messages = null;
+        try {
+            t = new Socket("192.168.2.122", 80);
+            messages = new DataOutputStream(t.getOutputStream());
+            messages.writeUTF(frequenz);
+            messages.close();
+            t.close();
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    };
 }
