a03fad37007368fcb6810e7a47b8ea3e5c92686c
Paul-Abel <59267403+Paul-Abel@users.noreply.github.com>
2020-03-17 18:42:25 +0100

    2 Seite für TV-Remote eingefügt

---
+++ app/src/main/java/com/example/tv_remote/tv_remote2.java
@@ -0,0 +1,14 @@
+package com.example.tv_remote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tv_remote2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tv_remote2);
+    }
+}
